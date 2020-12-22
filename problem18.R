046a2505490b63be8f3db125b9f2f7dc28521825
gustavosdo <gustavo.if.ufrj@gmail.com>
2020-12-22 19:58:58 -0300

    The unicity condition is correct now; the right answer was found

--- problem18.R
+++ problem18.R
@@ -41,10 +41,15 @@ triangle = list(c(75),
                 c(63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31),
                 c(04, 62, 98, 27, 23, 09, 70, 98, 73, 93, 38, 53, 60, 04, 23))
 
-triangle = list(c(1),
-                c(0, 1),
-                c(0, 0, 1),
-                c(1, 0, 0, 0))
+triangle_test = list(c(75),
+                     c(95, 64),
+                     c(17, 47, 82),
+                     c(18, 35, 87, 10))
+
+triangle_test_2 = list(c(1),
+                       c(0, 1),
+                       c(0, 0, 1),
+                       c(1, 0, 0, 0))
 
 # Maximization algorithm -------------------------------------------------------
 # I will try to solve this problem using a maximization algorithm. In order to
@@ -83,8 +88,9 @@ while(length(uni) < length(model$obj) - length(triangle)) {
       # Primary number
       uni_start[[r]][c] = 1
       # Neighbours
-      uni_start[[r+1]][c] = -1
-      uni_start[[r+1]][c+1] = -1
+      uni_start[[r+1]] = rep(1, length(uni_start[[r+1]]))
+      uni_start[[r+1]][c] = 0
+      uni_start[[r+1]][c+1] = 0
       # Returning result
       uni = c(uni, uni_start)
     }
@@ -96,18 +102,22 @@ mat_uni = matrix(unlist(uni, recursive = T), ncol = length(model$obj), byrow = T
 mat = rbind(mat_exc, mat_uni)
 
 model$modelsense = "max"
-model$A = mat_exc #mat #as(mat, 'sparseMatrix')
+model$A = mat #as(mat, 'sparseMatrix')
 #model$rhs = c(rep(1, nrow(mat_exc)), rep(0, nrow(mat_uni)))
-model$rhs = rep(1, nrow(mat_exc))
-model$sense = rep('=', nrow(mat_exc)) #rep('=', nrow(mat))
+model$rhs = rep(1, nrow(mat))
+model$sense = c(rep("=", nrow(mat_exc)), rep("<=", nrow(mat_uni))) #rep('=', nrow(mat))
 
 gurob = gurobi::gurobi(model = model, params = list(LogToConsole = 0))
 
 symph = Rsymphony::Rsymphony_solve_LP(obj = model$obj,
                                       mat = model$A,
-                                      dir = rep('==', nrow(model$A)),
+                                      dir = c(rep("==", nrow(mat_exc)), rep("<=", nrow(mat_uni))),
                                       rhs = model$rhs, types = "B",
                                       max = T)
 
 print("Gurobi solution: "); print(gurob$x)
 print("Rsymphony solution: "); print(symph$solution)
+
+
+print("Gurobi max obj value: "); print(gurob$objval)
+print("Rsymphony max obj value: "); print(symph$objval)
